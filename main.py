ff5e1f14537865312b8fcd21d3eef998d5926429
EDJINEDJA <edjinedjalaris@gmail.com>
2023-03-02 11:20:04 +0100

    My first commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from src.utils import utils
 
-parser = utils.Utils()
+parser = utils.Utils(2)
 print(parser.Load())
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -3,14 +3,18 @@ import os
 import re
 import glob
 from pathlib import Path
+import random
+
 
 class Utils():
 
-    def __init__(self) -> None:
+    def __init__(self,n) -> None:
         self.audios = os.path.realpath("./data/raw/*.wav")
+        self.n = n
 
     def Load(self):
-       
+        file_memory={}
+        cluster = []
         files = []
         for file in glob.glob(self.audios):
             files.append(file)
@@ -21,6 +25,23 @@ class Utils():
             base = re.findall("\d+.wav", item)
             parser = Path(" ".join(base))
             aud_idx.append(int(parser.stem))
-            
+            file_memory[int(parser.stem)]= item
+        max_aud_idx = max(aud_idx)
+        
+        k_cluster = int(max_aud_idx/self.n)
+      
+        for idx in range(0,k_cluster):
+            t=range(self.n*(idx) + 1 ,(idx+1)*self.n+1)
+            cluster.append([])
+            for i in t:
+                cluster[idx].append(i)
 
-        return aud_idx
+        for item in range(0,k_cluster):
+           cluster[item]=random.sample(cluster[item],len(cluster[item]))
+        
+        for idx , item in enumerate(cluster):
+            
+            for idx_ in item:
+                
+                yield(file_memory[idx_])
+        
